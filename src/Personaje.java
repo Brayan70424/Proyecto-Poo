4c38feef66c652f0f149b0544ed2d5fb6eb98ec7
Estudiante <estudiante@correo.com>
2026-09-05 12:46:03 +0000

    Agregar getters y setters de Personaje

--- src/Personaje.java
+++ src/Personaje.java
@@ -24,4 +24,39 @@ public class Personaje {
         this.estaVivo = estaVivo;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+        if (this.vida <= 0) {
+            this.estaVivo = false;
+        }
+    }
+
+    public boolean isEstaVivo() {
+        return estaVivo;
+    }
+
+    public void setEstaVivo(boolean estaVivo) {
+        this.estaVivo = estaVivo;
+    }
+
 }
