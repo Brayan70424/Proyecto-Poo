0b8e3e52ef58264d3a8f97131e19415de4a8818d
Estudiante <estudiante@correo.com>
2026-09-05 12:48:04 +0000

    Instanciar objetos de la superclase Personaje con distintos constructores

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,16 @@ public class Main {
         System.out.println(" SISTEMA DE PERSONAJES - VIDEOJUEGO");
         System.out.println("=================================================\n");
 
+        Personaje personajeVacio = new Personaje();
+        Personaje npcGenerico = new Personaje("Aldeano");
+        Personaje viajero = new Personaje("Viajero", 5);
+        Personaje narrador = new Personaje("El Cronista", 50, 999.0, true);
+
+        System.out.println("--- Personajes genericos (superclase) ---");
+        System.out.println(personajeVacio);
+        System.out.println(npcGenerico);
+        System.out.println(viajero);
+        System.out.println(narrador);
+
     }
 }
